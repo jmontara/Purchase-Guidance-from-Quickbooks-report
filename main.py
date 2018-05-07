f4dcba9373faa4e438d07356b08c900db9dca822
[jmontara] <[jmontara@earthlink.net]>
2018-05-07 07:20:42 -0400

    add import.functions.buildItemBoms

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ import datetime
 
 # classes 
 import classes.transaction 
-import classes.bom 
 import classes.indentedBom
 
 # functions
 import functions.readiiqr
 import functions.readissbi
 import functions.buildItems
+import functions.buildItemBoms
 
 
 
@@ -50,51 +50,17 @@ print "buildItems() yields", len(items), "items"
 # print "buildItems() yields", len(items), "items"
 # assert False
 
-functions.readissbi.readissbi(filename = "issbi.csv", items = items)
+functions.readissbi.readissbi(filename = "issbi.csv", 
+							  items = items)
 # for item in items:
 	# print item
 # assert False
 	
 			 
-def buildItemBoms(trans = transactions, items = items):
-	""" 
-	Reads list of transactions and items.  Updates items with
-	a bill of materials.
-	
-	Inputs:
-	trans		- list, list of Transaction objects
-	items		- list, list of Item objects
 
-	"""
-	for item in items:
-		
-		itemCreatingTransaction = item.getItemCreatingTransaction()
-		# skip item if not an assembly		
-		if itemCreatingTransaction == None:
-			continue
 
-		
-		itemName = itemCreatingTransaction.getItemName()
-		transNum = itemCreatingTransaction.getTnum()
-		transQty = itemCreatingTransaction.getQty()
-		itemName = itemCreatingTransaction.getItemDesc()
-		bom = classes.bom.Bom(itemName, itemName, transNum)		
-		level = "."
-		for t in trans:
-			# disregard unless a build
-			if not t.getType() == "Build Assembly":
-				continue
-			# disregard unless lower asy 
-			if not float(t.getQty()) < 0:
-				continue				
-			if t.getTnum() == transNum:
-				itemQty = float(t.getQty())/float(transQty)
-				itemQty = str(itemQty)[:5]
-				bom.addItem(t.getItemName(), t.getItemDesc(), 
-							itemQty, level)	
-		item.setBom(bom)
-
-buildItemBoms()
+functions.buildItemBoms.buildItemBoms(trans = transactions, 
+									  items = items)
 count = 0
 for item in items:
 	if not item.getBom() == None:
