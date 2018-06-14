e8669ec714f98283620087e863fbd999a51ed14a
[jmontara] <[jmontara@earthlink.net]>
2018-06-14 07:30:23 -0400

    refactor getshipments taking items

--- cycletimes.py
+++ cycletimes.py
@@ -10,7 +10,7 @@ class Shipment(object):
 	def __init__(self,startTransaction,endTransaction):
 		self.start = startTransaction
 		self.end = endTransaction
-		# difference in days between buy and shipment
+		# difference in days between start and end
 		diff = endTransaction.getDate() - startTransaction.getDate()
 		self.time = diff.days
 	def getCycleTime(self):
@@ -51,12 +51,12 @@ class Sell(Shipment):
 	def getDest(self):
 		return self.end.getName()
 		
-def getshipments(transactions):
+def getshipments(items):
 	"""
 	returns dictionaries of objects
 	
 	Inputs:
-	transactions - list of transaction objects
+	items - list of item objects
 	
 	Outputs:
 	A tuple of dictionaries pointing to shipments,
@@ -65,8 +65,7 @@ def getshipments(transactions):
 	buyShipmentsBySupplier
 				example: {supplier: [shipment1, shipment2, shipment3]}
 	buyShipmentsByCategory
-				example: {category: [shipment1, shipment2, shipment3]}
-				
+				example: {category: [shipment1, shipment2, shipment3]}				
 	sellShipmentsByItem
 				example: {item: [shipment1, shipment2, shipment3]}
 	sellShipmentsByCustomer
@@ -75,64 +74,62 @@ def getshipments(transactions):
 				example: {category: [shipment1, shipment2, shipment3]}
 				  
 	"""
-
+	#(4) use (2) to populate buyShipmentsBySupplier
+	##(5) use (2) to populate buyShipmentsByCategory
+	##(6) use (1) to populate sellShipmentsByItem
+	##(7) use (6) to populate sellShipmentsByCustomer
+	##(8) use (6) to populate sellShipmentsByCategory
+	##(9) generate summary stats like samples, lead times, covariance 		
+	
 	buyShipmentsByItem = {}
 	buyStartTransactionTypes = ['Purchase Order']
 	buyEndTransactionTypes = ['Bill', 'Item Receipt']
-	
-	#(1) populate item dictionary of transactions
-	#(2) use (1) to populate buyShipmentsByItem
-	#(4) use (2) to populate buyShipmentsBySupplier
-	#(5) use (2) to populate buyShipmentsByCategory
-	#(6) use (1) to populate sellShipmentsByItem
-	#(7) use (6) to populate sellShipmentsByCustomer
-	#(8) use (6) to populate sellShipmentsByCategory
-	
-	for transaction in transactions:
-		type = transaction.getType()
-		if type in buyStartTransactionTypes:
-			shiptransactions.append(transaction)
-		elif type in buyEndTransactionTypes:
-			buytransactions.append(transaction)
-	# print "buytransactions:", buytransactions
-	# print "shiptransactions:", shiptransactions
-	
-	# (1)look at most recent Purchase Order transaction.
-	def getDate(transaction):
-		return transaction.getDate()
-		
-	sortedbuytransactions = sorted(buytransactions, 
-								   key=getDate,
-								   reverse=True)
-	sortedshiptransactions = sorted(shiptransactions, 
-								   key=getDate,
-								   reverse=True)
-								   
-	for buy in sortedbuytransactions:
-		print "buy:", buy.getShortStr()
-		# (2)if received, the quantity will be zero
-		#    and a subsequent Item Receipt or Bill is expected.
-			
-		if buy.getQty() == '0':
-		
-			buyDte = buy.getDate()
-			
-			leadTime = datetime.timedelta(999)
-			for ship in sortedshiptransactions:
-				shipDte = ship.getDate()
-				thisLeadTime = shipDte - buyDte 
-				zeroLeadTime = shipDte - shipDte
-				print "buy:", buy.getShortStr()
-				print "ship:", ship.getShortStr()
-				print "thisLeadTime:", thisLeadTime, "/n"
-				if zeroLeadTime <= thisLeadTime < leadTime:
-					leadTime = thisLeadTime
-					buyTransaction = buy
-					shipTransaction = ship
-					
-			shipments.append(Shipment(buyTransaction,shipTransaction))
 
-	return shipments
+	for item in items:
+		startTransactions = []
+		endTransactions = []
+		buys = []
+		for transaction in item.getXactions():
+			type = transaction.getType()
+			if type in buyStartTransactionTypes:
+				startTransactions.append(transaction)
+			elif type in buyEndTransactionTypes:
+				endTransactions.append(transaction)
+		
+		# look at most recent start transaction.
+		def getDate(transaction):
+			return transaction.getDate()
+			
+		sortedStartTransactions = sorted(startTransactions, 
+									   key=getDate,
+									   reverse=True)
+		sortedEndTransactions = sorted(endTransactions, 
+									   key=getDate,
+									   reverse=True)
+									   
+		for buy in sortedStartTransactions:
+			# If received, the quantity will be zero
+			# and a subsequent Item Receipt or Bill is expected.
+			if buy.getQty() == '0':
+			
+				buyDte = buy.getDate()
+				
+				leadTime = datetime.timedelta(999)
+				for ship in sortedEndTransactions:
+					shipDte = ship.getDate()
+					thisLeadTime = shipDte - buyDte 
+					zeroLeadTime = shipDte - shipDte
+
+					if zeroLeadTime <= thisLeadTime < leadTime:
+						leadTime = thisLeadTime
+						startTransaction = buy
+						endTransaction = ship
+						
+				buys.append(Shipment(startTransaction,endTransaction))
+				
+		buyShipmentsByItem[item] = buys
+		
+	return buyShipmentsByItem
 		
 	
 if __name__ == "__main__":
@@ -151,82 +148,87 @@ if __name__ == "__main__":
 							"num","qty","soNum", "name","memo", "soDte")
 	print t
 	
-	transactions = []
 	
-	transactions.append(classes.transaction.Transaction("item", "desc", "11", 
+	
+	item.addXaction(classes.transaction.Transaction("item", "desc", "11", 
 					"Bill", datetime.date(2017,6,19)
 					, "num","11","soNum", "name","memo", "soDte")
 					)	
-	transactions.append(classes.transaction.Transaction("item", "desc", "11", 
+	item.addXaction(classes.transaction.Transaction("item", "desc", "11", 
 					"Bill", datetime.date(2017,6,19)
 					, "num","11","soNum", "name","memo", "soDte")
 					)	
-	transactions.append(classes.transaction.Transaction("item", "desc", "11", 
+	item.addXaction(classes.transaction.Transaction("item", "desc", "11", 
 					"Build Assembly", datetime.date(2017,7,19)
 					, "num","11","soNum", "name","memo", "soDte")
 					)		
-	transactions.append(classes.transaction.Transaction("item", "desc", "11", 
+	item.addXaction(classes.transaction.Transaction("item", "desc", "11", 
 					"Bill", datetime.date(2017,10,17)
 					, "num","11","soNum", "name","memo", "soDte")
 					)	
-	transactions.append(classes.transaction.Transaction("item", "desc", "11", 
+	item.addXaction(classes.transaction.Transaction("item", "desc", "11", 
 					"Bill", datetime.date(2017,10,17)
 					, "num","11","soNum", "name","memo", "soDte")
 					)	
-	transactions.append(classes.transaction.Transaction("item", "desc", "11", 
+	item.addXaction(classes.transaction.Transaction("item", "desc", "11", 
 					"Bill", datetime.date(2017,10,17)
 					, "num","11","soNum", "name","memo", "soDte")
 					)	
-	transactions.append(classes.transaction.Transaction("item", "desc", "11", 
+	item.addXaction(classes.transaction.Transaction("item", "desc", "11", 
 					"Build Assembly", datetime.date(2017,11,11)
 					, "num","11","soNum", "name","memo", "soDte")
 					)	
-	transactions.append(classes.transaction.Transaction("item", "desc", "11", 
+	item.addXaction(classes.transaction.Transaction("item", "desc", "11", 
 					"Bill", datetime.date(2017,11,28)
 					, "num","11","soNum", "name","memo", "soDte")
 					)	
-	transactions.append(classes.transaction.Transaction("item", "desc", "11", 
+	item.addXaction(classes.transaction.Transaction("item", "desc", "11", 
 					"Bill", datetime.date(2017,11,28)
 					, "num","11","soNum", "name","memo", "soDte")
 					)	
-	transactions.append(classes.transaction.Transaction("item", "desc", "11", 
+	item.addXaction(classes.transaction.Transaction("item", "desc", "11", 
 					"Build Assembly", datetime.date(2018,2,9)
 					, "num","11","soNum", "name","memo", "soDte")
 					)	
-	transactions.append(classes.transaction.Transaction("item", "desc", "11", 
+	item.addXaction(classes.transaction.Transaction("item", "desc", "11", 
 					"Item Receipt", datetime.date(2018,5,29)
 					, "num","11","soNum", "name","memo", "soDte")
 					)	
-	transactions.append(classes.transaction.Transaction("item", "desc", "11", 
+	item.addXaction(classes.transaction.Transaction("item", "desc", "11", 
 					"Build Assembly", datetime.date(2018,6,1)
 					, "num","11","soNum", "name","memo", "soDte")
 					)	
 
-	transactions.append(classes.transaction.Transaction("item", "desc", "11", 
+	item.addXaction(classes.transaction.Transaction("item", "desc", "11", 
 					"Purchase Order", datetime.date(2017,5,23)
 					, "num","0","soNum", "name","memo", "soDte")
 					)	
-	transactions.append(classes.transaction.Transaction("item", "desc", "11", 
+	item.addXaction(classes.transaction.Transaction("item", "desc", "11", 
 					"Purchase Order", datetime.date(2017,9,6)
 					, "num","0","soNum", "name","memo", "soDte")
 					)	
-	transactions.append(classes.transaction.Transaction("item", "desc", "11", 
+	item.addXaction(classes.transaction.Transaction("item", "desc", "11", 
 					"Purchase Order", datetime.date(2017,10,31)
 					, "num","0","soNum", "name","memo", "soDte")
 					)	
-	transactions.append(classes.transaction.Transaction("item", "desc", "11", 
+	item.addXaction(classes.transaction.Transaction("item", "desc", "11", 
 					"Purchase Order", datetime.date(2018,4,23)
 					, "num","0","soNum", "name","memo", "soDte")
 					)	
 
-			
+	items = [item]
+	
 	# for t in item.getXactions():
 		# print "\n\ntransactions in item:\n"	
 		# print t
 		
-	shipments = getshipments(transactions)
+	buyShipmentsByItem = getshipments(items)
 	
-	for shipment in shipments:
-		print shipment
+	print ""
+	for item in buyShipmentsByItem.keys():
+		print "item.getItemName():", item.getItemName()
+		for shipment in buyShipmentsByItem[item]:
+			print " shipment:", shipment
+
 
 	
